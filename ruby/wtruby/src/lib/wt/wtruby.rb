65a1ec599eb3f7d189f71a2a62e4b0e573ff36b3
Richard Dale <rdale@mardigras.(none)>
2009-02-03 12:43:19 +0000

    * Change the license of Wt::Ruby from GPL to LGPL 2 and update the license headers in the source files. See the LICENSES and AUTHORS files for more details.

--- ruby/wtruby/src/lib/wt/wtruby.rb
+++ ruby/wtruby/src/lib/wt/wtruby.rb
@@ -1,20 +1,25 @@
 =begin
-/***************************************************************************
-                          wtruby.rb  -  description
-                             -------------------
-    begin                : Fri Jul 4 2003 (derived from the qtruby.rb source)
-    copyright            : (C) 2003-2008 by Richard Dale
-    email                : richard.j.dale@gmail.com
- ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+     wtruby.rb  -  derived from the qtruby.rb source in QtRuby
+
+/*
+ *   Copyright 2003-2009 by Richard Dale <richard.j.dale@gmail.com>
+
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 =end
 
 module Wt
--- ruby/wtruby/src/marshall_macros.h
+++ ruby/wtruby/src/marshall_macros.h
@@ -1,20 +1,26 @@
 /***************************************************************************
   marshall_macros.h  -  Useful template based marshallers for QLists, QVectors
                         and QLinkedLists
-                             -------------------
-    begin                : Thurs Jun 8 2008
-    copyright            : (C) 2008 by Richard Dale
-    email                : richard.j.dale@gmail.com
  ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ *   Copyright 2008-2009 by Richard Dale <richard.j.dale@gmail.com>
+
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #ifndef MARSHALL_MACROS_H
 #define MARSHALL_MACROS_H
