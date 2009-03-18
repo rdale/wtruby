d20d2ef90f5b067b77de1e8f25f081641b1f412b
Richard Dale <rdale@baldhead.(none)>
2009-03-18 19:51:22 +0000

    * The bobsmith example runs fine under Wt 3.0

---
+++ ruby/wtruby/examples_3_0/bobsmith/bobsmith.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+#
+# Copyright (C) 2008 Emweb bvba, Kessel-Lo, Belgium.
+#
+# See the LICENSE file for terms of use.
+#
+# Converted to Ruby by Richard Dale
+
+require 'wt'
+
+Wt::WRun(ARGV) do |env|
+  app = Wt::WApplication.new(env)
+
+  Wt::WText.new("Name: ", app.root)
+  edit = Wt::WInPlaceEdit.new("Bob Smith", app.root)
+  edit.styleClass = "inplace"
+
+  app.styleSheet.addRule("*.inplace span:hover", "background-color: gray")
+
+  app
+end
+
+# kate: space-indent on; indent-width 2; replace-tabs on; mixed-indent off;
